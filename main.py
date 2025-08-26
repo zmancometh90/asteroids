979f5f47141c5a20e569495b8a3f20b74e1ab042
zmancometh90 <zachary.mashaw@gmail.com>
2025-08-25 20:35:28 -0400

    Added game loop

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+        screen.fill("black")
+        pygame.display.flip()
+    
+
+
+if __name__ == "__main__":
+    main()
